2b094e7672edd64c59ac77901f1c0fba48a28881
drsky <contact@drsky.ovh>
2023-12-09 22:30:35 +0100

    refactor: update project configuration for library creation
    Modified the project's setup to support the development of a new library. This includes adjustments in build scripts, dependencies, and directory structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '../lib/main';
+
 function App() {
-  return <h1>Datepicker</h1>;
+  return (
+    <>
+      <h1>Datepicker</h1>
+      <Button />
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react-swc';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { fileURLToPath } from 'url';
+import { glob } from 'glob';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}')
+          .map((file) => [
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
+  },
 });
