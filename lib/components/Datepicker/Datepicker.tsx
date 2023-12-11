6fe3e3d8568a14d0b4ee68a4cf607a5dd0b37e7f
drsky <contact@drsky.ovh>
2023-12-11 10:31:10 +0100

    feat: Implement base input component for datepicker

---
+++ lib/components/Datepicker/Datepicker.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import styles from './Datepicker.module.css';
+import Calendar from '../Calendar/Calendar';
+import isValidDateString from '../../utils/date';
+
+type DatepickerProps = {
+  date: string;
+  onChange: React.Dispatch<React.SetStateAction<string>>;
+};
+
+function Datepicker({ date, onChange }: DatepickerProps) {
+  const [displayCalendar, setDisplayCalendar] = useState(false);
+  const [lastValideDate, setLastValideDate] = useState(isValidDateString(date) ? date : '');
+
+  function handleBlur() {
+    if (isValidDateString(date)) {
+      setLastValideDate(date);
+    } else {
+      onChange(lastValideDate);
+    }
+  }
+
+  function handleClick(event: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+    event.preventDefault();
+    setDisplayCalendar((lastState) => (!lastState ? !lastState : lastState));
+  }
+
+  return (
+    <div className={styles.inputWrapper}>
+      <input
+        type='text'
+        value={date}
+        onChange={(event) => onChange(event.target.value)}
+        onClick={handleClick}
+        onBlur={handleBlur}
+        pattern='^(1[0-2]|0?[1-9])(\/|-)(3[01]|[12][0-9]|0?[1-9])(\/|-)([0-9]{2})?[0-9]{2}$'
+        title='Enter the date in this format: MM/DD/YYYY or MM-DD-YYYY'
+      />
+      {displayCalendar && <Calendar />}
+    </div>
+  );
+}
+
+export default Datepicker;
