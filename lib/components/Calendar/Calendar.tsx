528486c4d8f075e1bf189c8c006892a672b36d5d
drsky <contact@drsky.ovh>
2023-12-25 22:12:11 +0100

    refactor(calendar): Replace direct component rendering with children prop usage
    - Modify the component structure to utilize children props instead of direct component returns.

--- lib/components/Calendar/Calendar.tsx
+++ lib/components/Calendar/Calendar.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useMemo, useState } from 'react';
-import styles from './Calendar.module.css';
+import { PropsWithChildren, useEffect, useMemo, useState } from 'react';
+import CalendarContext from './context';
 import MonthSelection from './MonthSelection';
 import WeekDayLabels from './WeekDayLabels';
 import YearSelection from './YearSelection';
-import { parseDateString } from '../../utils/date';
-import CalendarContext from './context';
 import Days from './days';
+import { parseDateString } from '../../utils/date';
+import styles from './Calendar.module.css';
 
-type CalendarProps = {
+type CalendarProps = PropsWithChildren<{
   date: string;
-  onClickNewDate: React.Dispatch<React.SetStateAction<string>>;
-};
+  onClickNewDate: (newDate: string) => void;
+}>;
 
-function Calendar({ date, onClickNewDate }: CalendarProps) {
+function Calendar({ date, onClickNewDate, children }: CalendarProps) {
   const [calendarDate, setCalendarDate] = useState(() => parseDateString(date));
 
+  // TODO: extraire la logique de validation de date dans un hook
   useEffect(() => {
     setCalendarDate(parseDateString(date));
   }, [date]);
@@ -30,18 +31,14 @@ function Calendar({ date, onClickNewDate }: CalendarProps) {
 
   return (
     <CalendarContext.Provider value={contextValue}>
-      <div className={styles.calendar}>
-        <div className={styles.header}>
-          <MonthSelection />
-          <YearSelection />
-        </div>
-        <WeekDayLabels />
-        <Days />
-      </div>
+      <div className={styles.calendar}>{children}</div>
     </CalendarContext.Provider>
   );
 }
 
 Calendar.MonthSelection = MonthSelection;
+Calendar.YearSelection = YearSelection;
+Calendar.WeekDayLabels = WeekDayLabels;
+Calendar.Days = Days;
 
 export default Calendar;
